1643c79ebc4692e47187e4c4d14592255bfccb42
Alexander Smolowitz <alexander.smolowitz@city.ac.uk>
2025-01-22 19:20:13 +0000

    Changes

--- BasicDemo/src/game/Game.java
+++ BasicDemo/src/game/Game.java
@@ -6,77 +6,68 @@ import org.jbox2d.common.Vec2;
 
 import javax.swing.JFrame;
 
-import java.awt.*;
-import java.io.IOException;
-import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.UnsupportedAudioFileException;
 
 /**
  * Your main game entry point
  */
 public class Game {
+    float[] studentPos = {7, -9};
+    float[] bookPos = {-8, 7.5f};
+    StaticBody ground;
+    StaticBody platform1;
+    DynamicBody student;
+    DynamicBody book;
 
-
-    /** Initialise a new Game. */
+    /**
+     * Initialise a new Game.
+     */
     public Game() {
-
-        //1. make an empty game world
         World world = new World();
-
-        //2. populate it with bodies (ex: platforms, collectibles, characters)
-
         //make the ground
         Shape shape = new BoxShape(11, 0.5f);
-        StaticBody ground = new StaticBody(world, shape);
+        ground = new StaticBody(world, shape);
         ground.setPosition(new Vec2(0f, -11.5f));
 
         // make a platform
         Shape platformShape = new BoxShape(3.5f, 0.5f);
-        StaticBody platform1 = new StaticBody(world, platformShape);
+        platform1 = new StaticBody(world, platformShape);
         platform1.setPosition(new Vec2(-8, 5.5f));
 
 
         //make a character (with an overlaid image)
-        Shape studentShape = new BoxShape(1,2);
-        DynamicBody student = new DynamicBody(world, studentShape);
-        student.setPosition(new Vec2(7,-9));
+        Shape studentShape = new BoxShape(1, 2);
+        student = new DynamicBody(world, studentShape);
+        student.setPosition(new Vec2(studentPos[0], studentPos[1]));
         student.addImage(new BodyImage("data/student.png", 4));
 
+        Shape bookShape = new BoxShape(2, 1.5f);
+        book = new DynamicBody(world, bookShape);
+        book.setPosition(new Vec2(bookPos[0], bookPos[1]));
+        book.addImage(new BodyImage("data/books.png", 3));
 
-        //3. make a view to look into the game world
         UserView view = new UserView(world, 500, 500);
 
-
-        //optional: draw a 1-metre grid over the view
-        //view.setGridResolution(1);
-
-
-        //4. create a Java window (frame) and add the game
-        //   view to it
+        view.setGridResolution(1);
         final JFrame frame = new JFrame("City Game");
         frame.add(view);
-
-        // enable the frame to quit the application
-        // when the x button is pressed
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationByPlatform(true);
-        // don't let the frame be resized
         frame.setResizable(false);
-        // size the frame to fit the world view
         frame.pack();
-        // finally, make the frame visible
         frame.setVisible(true);
-
-        //optional: uncomment this to make a debugging view
-       //  JFrame debugView = new DebugViewer(world, 500, 500);
-
-        // start our game world simulation!
+        student.setAlwaysOutline(true);
+        book.setAlwaysOutline(true);
         world.start();
     }
-
+    public void jump(float vec1, float vec2) {
+        student.setLinearVelocity(new Vec2(vec1, vec2));
+    }
     /** Run the game. */
     public static void main(String[] args) {
-
-        new Game();
+        boolean space = true;
+        Game game = new Game();
+        if (space == true) {
+            game.jump(0, 10);
+        }
     }
 }
