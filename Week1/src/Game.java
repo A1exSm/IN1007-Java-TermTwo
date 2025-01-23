4aee761c8b5b1bd5679198fc64d00cbd6dccfe9a
Alexander Smolowitz <alexander.smolowitz@city.ac.uk>
2025-01-23 19:50:27 +0000

    Week1

---
+++ Week1/src/Game.java
@@ -0,0 +1,27 @@
+import city.cs.engine.*;
+import city.cs.engine.Shape;
+import org.jbox2d.common.Vec2;
+import javax.swing.*;
+public class Game {
+    public Game() {
+        World world = new World();
+        // user view
+        UserView view = new UserView(world, 500, 500);
+        view.setGridResolution(1);
+        // Jframe
+        final JFrame frame = new JFrame("Game");
+        this.jFrameHandler(frame, view);
+        // world start
+        world.start();
+    }
+
+    private void jFrameHandler(JFrame frame, UserView view) {
+        frame.add(view);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationByPlatform(true);
+        frame.setResizable(true);
+        frame.pack();
+        frame.setVisible(true);
+        frame.setVisible(true);
+    }
+}
